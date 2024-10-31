2f885b60a44be6246a1ba3b0335b1d8f99bbe727
Iyaan Azeez <iyaanazeez757@gmail.com>
2024-10-31 10:01:01 +0500

    Rewrite port access functions

--- src/virtual.h
+++ src/virtual.h
@@ -30,16 +30,82 @@
 #include <cstring>
 #include <cstdio>
 
-const char sIO_FILE[] = "c:/emu8086.io";
+#define FILE_ERR "Cannot open C:/emu8086.io\n"
+#define IO_NOT_INIT_ERR "Call init_virtual_device first\n"
 
-unsigned char READ_IO_BYTE(long lPORT_NUM)
-{
+const char IO_FILE_PATH[] = "c:/emu8086.io";
+FILE *ioFileHandler = NULL;
+
+inline void is_virtual_device_initalized() {
+	if (ioFileHandler == NULL) {
+		fprintf(stderr, IO_NOT_INIT_ERR);
+		exit(EXIT_FAILURE);
+	}
+}
+
+// Get a handle to the file. Reduce repeated fopen() kernel calls
+int init_virtual_device() {
+	ioFileHandler = fopen(IO_FILE_PATH,"r+");
+	if (ioFileHandler == NULL) {
+		fprintf(stderr, FILE_ERR);
+		return -1;
+	}
+	return 0;
+}
+
+void free_virtual_device() {
+	is_virtual_device_initalized();
+	fclose(ioFileHandler);
+}
+
+int read_port_byte(long port) {
+	is_virtual_device_initalized();
+	FILE *fp = ioFileHandler;
+	int ch;
+	fseek(fp, port, SEEK_SET);
+    ch = fgetc(fp);
+	return ch;
+}
+
+void write_port_byte(long port, unsigned char uValue) {
+	is_virtual_device_initalized();
+	FILE *fp = ioFileHandler;
+	fseek(fp, port, SEEK_SET);
+	fputc(uValue, fp); 
+}
+
+short int read_port_word(long port) {
+	is_virtual_device_initalized();
+	short int word;
+	unsigned char tb1;
+	unsigned char tb2;
+
+	tb1 = read_port_byte(port);
+	tb2 = read_port_byte(port + 1);
+
+	// Convert 2 bytes to a 16 bit word:
+	word = tb2;
+    word = word << 8;
+	word = word + tb1;
+
+	return word;
+}
+
+void write_port_word(long port, short int iValue) {
+	is_virtual_device_initalized();
+	write_port_byte (port, iValue & 0x00FF);
+	write_port_byte (port + 1, (iValue & 0xFF00) >> 8);
+}
+
+/* Old IO functions from emu8086*/
+
+unsigned char READ_IO_BYTE(long lPORT_NUM) {
 	unsigned char tb;
 
 	char buf[500];
 	unsigned int ch;
 
-	strcpy(buf, sIO_FILE);
+	strcpy(buf, IO_FILE_PATH);
 
 	FILE *fp;
 
@@ -82,7 +148,7 @@ void WRITE_IO_BYTE(long lPORT_NUM, unsigned char uValue)
 	char buf[500];
 	unsigned int ch;
 
-	strcpy(buf, sIO_FILE);
+	strcpy(buf, IO_FILE_PATH);
 
 	FILE *fp;
 
